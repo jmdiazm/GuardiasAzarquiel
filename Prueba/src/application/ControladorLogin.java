4ef26b7049220cdda0c48da8965cdfd8a9f0850e
Chen <Chen@Chen-PC>
2015-05-06 18:39:28 +0200

    Actualizacion Boton

--- Prueba/src/application/ControladorLogin.java
+++ Prueba/src/application/ControladorLogin.java
@@ -24,7 +24,7 @@ public class ControladorLogin extends Main{
     void enviar(ActionEvent event) {
     	if ((usuario.getText()).equals(contraseña.getText())) {
     		estado.setText("Contraseña Correcta");
-
+    		/*Prueba del boton*/
     	}
     	else if (!(usuario.getText()).equals(contraseña.getText())) {
     		estado.setText("Error, la contraseña y el nombre no coinciden");
